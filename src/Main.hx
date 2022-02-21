6e20bd7b491b633fbe9dc75f9fd0c9c7e17de0f6
Jones-SnowCone <joelwhyte02@gmail.com>
2022-02-21 02:23:42 -0500

    changing to hashlink

--- src/Main.hx
+++ src/Main.hx
@@ -1,7 +1,7 @@
 class Main extends hxd.App {
     override function init() {
         var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
-        tf.text = "Hello World !";
+        tf.text = "Hello Hashlink !";
     }
     static function main() {
         new Main();
